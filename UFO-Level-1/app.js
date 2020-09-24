80ec3d8e5503f93c55561f5f6499833fd313e6a7
bonem97 <bonem1997@gmail.com>
2020-09-23 21:59:53 -0400

    decluttered

--- UFO-Level-1/app.js
+++ UFO-Level-1/app.js
@@ -1,24 +1,21 @@
 // app.js 
 
 // from data.js
+// name variables
 var tableData = data;
 var tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-
-// name variables
-var Search_Date = d3.select("#searchDate");
-var btn_Search = d3.select("#btnSearch");
-var Table_Body = d3.select("tbody");
-var btn_Reset = d3.select("#btnReset");
-
 // Table_Reload
-var Table_Reload);
-	
-	// adds a new row for each row of Data
-	Data.forEach(dataRow => { 
-		var tblRow = tbody.append("tr");  
-		
-		// adds columns
-		tblColumns.forEach(column => tblRow.append("td").text(dataRow[column]))
-	});
-}
+var Table_Reload = (Data) => {
+	tbody.html("");
+		// adds a new row for each row of Data
+		Data.forEach(dataRow => { 
+			var tblRow = tbody.append("tr");  
+			
+			// adds columns
+			tblColumns.forEach(column => tblRow.append("td").text(dataRow[column]))
+		});
+};
+
+// Use Table_Reload
+Table_Reload
