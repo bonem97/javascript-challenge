09c7fb9d46164522bbbe5cf217ad46f519b142fa
bonem97 <bonem1997@gmail.com>
2020-09-27 23:18:39 -0400

    reworked functions

--- UFO-Level-1/app.js
+++ UFO-Level-1/app.js
@@ -3,24 +3,34 @@
 // from data.js
 // name variables
 let tableData = data;
+let tbody = d3.select("tbody")
 let tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
-
+ 
 function coll_add {
 	tblColumns.forEach(column => 
 		tblRow.append("td").text(dataRow[column]))
 }
 
 // Table_Reload
-let Table_Reload = (Data) => {
-	tbody.html("");
-		// adds a new row for each row of Data
-		Data.forEach(dataRow => { 
-			var tblRow = tbody.append("tr");  
-			
-			// adds columns
-			coll_add
-		});
-};
+let Table_Reload = {data.forEach(function(UFOinfo) {
+	// append rows and information according to entrances in data
+    console.log(UFOinfo);
+    let row = tbody.append("tr");
+    Object.entries(UFOinfo).forEach(
+		function([key, value]) {
+    	let cell = row.append("td");
+    	cell.text(value);
+    })
+});
 
 // Use Table_Reload
 Table_Reload
+
+function UFO_Finder() {
+	d3.event.PreventDefault();
+
+	let filtered_list = tableData.filter(
+		UFO => UFO.datetime === d3.select("#datetime").property("value");
+	);
+	
+}
